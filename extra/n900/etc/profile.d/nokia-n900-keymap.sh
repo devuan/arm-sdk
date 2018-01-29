c8f1a3b9c52583a348b2d0e6acc038b4c7d9a039
parazyd <parazyd@dyne.org>
2018-01-29 15:16:29 +0100

    n900-root: Load keymap in Xorg as well.

--- extra/n900/etc/profile.d/nokia-n900-keymap.sh
+++ extra/n900/etc/profile.d/nokia-n900-keymap.sh
@@ -1,1 +1,6 @@
-/bin/busybox loadkmap < /etc/nokia-n900.kmap
+# Setup Nokia N900 keymap either on console or Xorg
+if [ -n "$DISPLAY" ]; then
+	setxkbmap -rules evdev -model nokiarx51 -layout us -variant ",qwerty"
+else
+	/bin/busybox loadkmap < /etc/nokia-n900.kmap
+fi
